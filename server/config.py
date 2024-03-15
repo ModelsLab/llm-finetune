deae3df0c0fd55fe692338ff3eeb23ce8d2551cd
Tanmaypatil123 <tanmaypatil3151@gmail.com>
2024-03-15 18:11:45 +0530

    removed creds

--- server/config.py
+++ server/config.py
@@ -5,7 +5,10 @@
 # CLASS_DIR_couple = "class_images_couple"
 # CLASS_DIR = "class_images"
 # OUTPUT_DIR = "./output/trained-model"
+import os
+from dotenv import load_dotenv
 
-HF_TOKEN = "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"
+load_dotenv()
+HF_TOKEN = os.getenv("HF_TOKEN")
 HF_API_URL = "https://huggingface.co/api/models/"
 MODELS_PATH = "system_models/"
